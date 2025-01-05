9b9a0b631a4ad3a3a686e913fdb40d77ee03b8b9
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-01-05 21:06:57 +0000

    add user schema

---
+++ src/utils/models/user.model.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.models.Order || mongoose.model("User", UserSchema);
