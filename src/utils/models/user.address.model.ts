2edec336efcaef03f9819aaf554eeef9c377b132
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-01-12 13:01:21 +0530

    complete user address model

--- src/utils/models/user.address.model.ts
+++ src/utils/models/user.address.model.ts
@@ -1,8 +1,35 @@
 import mongoose, { Schema } from "mongoose";
 
 const UserAddressSchema = new Schema({
-  
-});
+  name: {
+    type: String,
+    required: [true, "Name is required"]
+  },
+  mobile: {
+    type: Number,
+    required: [true, "Number is required"]
+  },
+  state: {
+    type: String,
+    required: [true, "State is required"]
+  },
+  city: {
+    type: String,
+    required: [true, "City is required"]
+  },
+  nearByRoadStreet: {
+    type: String,
+    required: [true, "nearByRoadStreet is required"]
+  },
+  pincode: {
+    type: Number,
+    required: [true, "Pincode is required"]
+  },
+  addressBelongTo: {
+    type: String,
+    required: [true, "addressBelongTo undefined"]
+  }
+}, { timestamps: true });
 
 export const UserAddress =
   mongoose.models.UserAddress ||
